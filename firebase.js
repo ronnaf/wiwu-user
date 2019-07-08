0ca669eff298684734cd2c3d0b909bd9d5233af9
Mark <jardenilmark@gmail.com>
2019-07-09 00:43:45 +0800

    feat: firebase

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBwvfQvIxe14wJMbOvSoAGLeaG3t5KSsfM',
+  authDomain: 'weewoo-b9408.firebaseapp.com',
+  databaseURL: 'https://weewoo-b9408.firebaseio.com',
+  projectId: 'weewoo-b9408',
+  storageBucket: '',
+  messagingSenderId: '363729210043',
+  appId: '1:363729210043:web:47b0c446dc062344'
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const auth = firebaseApp.auth()
+const firestore = firebaseApp.firestore()
+// Initialize Firebase
+export { auth, firestore }
