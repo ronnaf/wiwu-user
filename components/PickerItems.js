daf81079b049d8f52a9f3d1c27cbb3aa82d64c07
Jess Lanchinebre <jevi.lanchinebre@gmail.com>
2019-08-04 20:09:56 +0800

    fix: picker items component
    fix useState callback warning & value change reset other values

--- components/PickerItems.js
+++ components/PickerItems.js
@@ -3,27 +3,28 @@ import { Item as FormItem, Picker, Label } from 'native-base'
 
 // NOTE: picker placeholder not supported by android
 const PickerItems = ({ items }) => {
-  const [selected, setSelected] = useState({})
-  const pickerItems = items.map(item => (
-    <Fragment key={items.indexOf(item)}>
-      <Label>{item.question}</Label>
-      <FormItem picker>
-        <Picker
-          note
-          mode='dropdown'
-          selectedValue={selected}
-          onValueChange={setSelected}>
-          {item.answers.map(answer => (
-            <Picker.Item
-              label={answer}
-              value={answer}
-              key={item.answers.indexOf(answer)}
-            />
-          ))}
-        </Picker>
-      </FormItem>
-    </Fragment>
-  ))
+  const pickerItems = items.map(item => {
+    const [selected, setSelected] = useState({})
+    return (
+      <Fragment key={items.indexOf(item)}>
+        <Label>{item.question}</Label>
+        <FormItem picker>
+          <Picker
+            mode='dropdown'
+            selectedValue={selected}
+            onValueChange={selected => setSelected(selected)}>
+            {item.answers.map(answer => (
+              <Picker.Item
+                label={answer}
+                value={answer}
+                key={item.answers.indexOf(answer)}
+              />
+            ))}
+          </Picker>
+        </FormItem>
+      </Fragment>
+    )
+  })
   return pickerItems
 }
 
