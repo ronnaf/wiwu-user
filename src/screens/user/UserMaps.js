06131dcca1a05d722f38e93c2f45961d8b242184
Mark <jardenilmark@gmail.com>
2019-08-24 15:41:18 +0800

    feat: add search bar for google maps and fixed some ui issues

--- src/screens/user/UserMaps.js
+++ src/screens/user/UserMaps.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Content } from 'native-base'
+import { Container } from 'native-base'
 
 import Map from '../../components/Map'
 import GenericHeader from '../../components/GenericHeader'
@@ -9,13 +9,7 @@ const UserMaps = props => {
   return (
     <Container>
       <GenericHeader title='Maps' type='drawer' />
-      <Content
-        padder
-        style={{
-          height: '80%'
-        }}>
-        <Map />
-      </Content>
+      <Map />
       <Footer active='map' />
     </Container>
   )
