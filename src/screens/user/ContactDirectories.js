f6a08e4830dc23da103d1367283bd77b37e8b52e
Mark <jardenilmark@gmail.com>
2019-08-24 21:35:20 +0800

    refactor(contactDirectories): change modal ui

--- src/screens/user/ContactDirectories.js
+++ src/screens/user/ContactDirectories.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { StyleSheet, Modal, TouchableHighlight } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { createAction } from 'redux-actions'
 import {
   Container,
@@ -16,8 +16,11 @@ import {
   Item,
   Segment,
   Button,
-  View
+  View,
+  Card,
+  CardItem
 } from 'native-base'
+import Modal from 'react-native-modal'
 
 import GenericHeader from '../../components/GenericHeader'
 import Footer from '../../components/Footer'
@@ -47,43 +50,46 @@ const ContactDirectories = props => {
 
   return (
     <Container>
-      <Modal
-        animationType={'fade'}
-        transparent={false}
-        visible={isModalVisible}>
-        <View style={{ marginTop: 22 }}>
-          <View>
-            {/* KAYA MO NI RONS */}
-            <Text>Name: {modalContent.name}</Text>
-            <Text>Address: {modalContent.address}</Text>
-            <Text>Numbers: {modalContent.numbers.join()}</Text>
-            <Button
-              onPress={() => {
-                const { latitude, longitude } = modalContent.location
-                dispatch(
-                  createAction(EDIT_PIN_COORDINATES)({
-                    latitude,
-                    longitude
-                  })
-                )
-                dispatch(
-                  createAction(EDIT_REGION_COORDINATES)({
-                    latitude,
-                    longitude
-                  })
-                )
-                setModalVisibility(false)
-                props.navigation.navigate('UserMaps')
-              }}>
-              <Text>Find in Maps</Text>
-            </Button>
-            <TouchableHighlight
-              onPress={() => {
-                setModalVisibility(false)
-              }}>
-              <Text>Hide Modal</Text>
-            </TouchableHighlight>
-          </View>
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.modalContent}>
+          <Card>
+            <CardItem header>
+              <Text>Name: {modalContent.name}</Text>
+            </CardItem>
+            <CardItem>
+              <Text>Address: {modalContent.address}</Text>
+            </CardItem>
+            <CardItem>
+              <Text>Numbers: {modalContent.numbers.join()}</Text>
+            </CardItem>
+            <CardItem footer>
+              <Button
+                block
+                success
+                onPress={() => {
+                  const { latitude, longitude } = modalContent.location
+                  dispatch(
+                    createAction(EDIT_PIN_COORDINATES)({
+                      latitude,
+                      longitude
+                    })
+                  )
+                  dispatch(
+                    createAction(EDIT_REGION_COORDINATES)({
+                      latitude,
+                      longitude
+                    })
+                  )
+                  setModalVisibility(false)
+                  props.navigation.navigate('UserMaps')
+                }}>
+                <Text>Find In Maps</Text>
+              </Button>
+              <Button block danger onPress={() => setModalVisibility(false)}>
+                <Text>Hide Modal</Text>
+              </Button>
+            </CardItem>
+          </Card>
         </View>
       </Modal>
 
@@ -160,6 +166,18 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     fontWeight: 'bold',
     fontSize: 24
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+    borderColor: 'rgba(0, 0, 0, 0.1)'
   }
 })
 
