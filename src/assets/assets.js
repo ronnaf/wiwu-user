bf2833905357b37af2238e272dfabbde7e18e130
ronnamaeffirmo <ronnwiseley@gmail.com>
2019-09-11 02:55:01 +0800

    feat(Camera helper): set up camera helper, settings camera

---
+++ src/assets/assets.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    logo: require('./images/wiwu-logo.png'),
+    logo2: require('./images/wiwu-logo-2.png'),
+    defaultAvatar: require('./images/generic-user.png')
+  },
+  fonts: {}
+}
--- src/assets/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  images: {
-    logo: require('./images/wiwu-logo.png'),
-    logo2: require('./images/wiwu-logo-2.png')
-  },
-  fonts: {}
-}
