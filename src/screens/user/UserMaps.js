2223a635476feb75874a0e57d614bce9a58a778c
Mark <jardenilmark@gmail.com>
2019-09-12 13:24:30 +0800

    refactor(Maps): add try catch to listener

--- src/screens/user/UserMaps.js
+++ src/screens/user/UserMaps.js
@@ -11,29 +11,35 @@ import Footer from '../../components/Footer'
 
 import { GET_ALL_EMERGENCIES } from '../../actions/emergency/emergency.constants'
 
+import showToast from '../../helpers/toast.helper'
+
 const UserMaps = () => {
   const dispatch = useDispatch()
   const emergencies = useSelector(e => e.emergency.list)
 
   useEffect(() => {
-    const listenerRef = firestore
-      .collection('emergencies')
-      .where('status', '==', 'PENDING')
-      .onSnapshot(snapshot => {
-        const emergencies = snapshot.docs.map(e => {
-          const data = e.data()
+    try {
+      const listenerRef = firestore
+        .collection('emergencies')
+        .where('status', '==', 'PENDING')
+        .onSnapshot(snapshot => {
+          const emergencies = snapshot.docs.map(e => {
+            const data = e.data()
 
-          return {
-            id: e.id,
-            location: data.location,
-            department: data.department
-          }
+            return {
+              id: e.id,
+              location: data.location,
+              department: data.department
+            }
+          })
+          dispatch(createAction(GET_ALL_EMERGENCIES)(emergencies))
         })
-        dispatch(createAction(GET_ALL_EMERGENCIES)(emergencies))
-      })
 
-    return function cleanup() {
-      listenerRef()
+      return function cleanup() {
+        listenerRef()
+      }
+    } catch (e) {
+      showToast('No connection found')
     }
   }, [])
 
