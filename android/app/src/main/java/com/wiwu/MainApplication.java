840ef0f05fa5408629e7907e1c37ac4185c37af4
ronnamaeffirmo <ronnwiseley@gmail.com>
2019-09-19 03:27:56 +0800

    chore(xcode): fix twilio xcode build error

--- android/app/src/main/java/com/wiwu/MainApplication.java
+++ android/app/src/main/java/com/wiwu/MainApplication.java
@@ -3,6 +3,7 @@ package com.wiwu;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.twiliorn.library.TwilioPackage;
@@ -45,6 +46,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
           new ReactNativeOneSignalPackage(),
           new VectorIconsPackage(),
           new TwilioPackage(),
