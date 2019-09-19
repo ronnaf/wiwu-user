0146cfbb1be0ac82ab1940e579de1497a9943421
ronnamaeffirmo <ronnwiseley@gmail.com>
2019-09-19 15:09:20 +0800

    fix(sendRequest): allow image upload if user is online

--- src/actions/emergency/sendRequest.action.js
+++ src/actions/emergency/sendRequest.action.js
@@ -1,12 +1,8 @@
 import { WIWU_OFFLINE_EMERGENCY_ARRAY } from './emergency.constants'
 import { SCREEN_LOADING } from '../user/user.constants'
-
 import { firestore, firebase } from '../../firebase'
-
 import showToast from '../../helpers/toast.helper'
-
 import NavigationService from '../../navigation/NavigationService'
-
 import { PermissionsAndroid, NativeModules, Platform } from 'react-native'
 import { SECRET_KEY } from 'react-native-dotenv'
 import { createAction } from 'redux-actions'
@@ -26,101 +22,119 @@ export function sendRequestAction(values) {
         map: { pinCoordinates }
       } = getState()
       const simpleCrypto = new SimpleCrypto(SECRET_KEY)
+      const smsReceiver = '09177456123'
 
       dispatch(createAction(SCREEN_LOADING)(true))
-      await uploadAsset(
-        'emergency-assets',
-        'image',
-        values.media,
-        async url => {
-          const payload = {
-            ...values,
-            userId: isOffline ? uid : firestore.doc(`users/${uid}`),
-            location: pinCoordinates,
-            responderId: null,
-            adminId: null,
-            priority: 'UNDETERMINED',
-            status: 'PENDING',
-            date: new Date(),
-            media: url
+
+      const payload = {
+        ...values,
+        userId: isOffline ? uid : firestore.doc(`users/${uid}`),
+        location: pinCoordinates,
+        responderId: null,
+        adminId: null,
+        priority: 'UNDETERMINED',
+        status: 'PENDING',
+        date: new Date(),
+        media: null
+      }
+
+      if (isOffline) {
+        if (Platform.OS === 'android') {
+          // works only for android
+          const DirectSms = NativeModules.DirectSms
+          const permissionMessage = {
+            title: 'wiwu SMS Permission',
+            message:
+              'wiwu needs access to your inbox so you can send messages in the background',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK'
           }
 
-          if (isOffline) {
-            if (Platform.OS === 'android') {
-              // works only for android
-              const DirectSms = NativeModules.DirectSms
-              const granted = await PermissionsAndroid.request(
-                PermissionsAndroid.PERMISSIONS.SEND_SMS,
-                {
-                  title: 'wiwu SMS Permission',
-                  message:
-                    'wiwu needs access to your inbox ' +
-                    'so you can send messages in the background',
-                  buttonNeutral: 'Ask Me Later',
-                  buttonNegative: 'Cancel',
-                  buttonPositive: 'OK'
-                }
-              )
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.SEND_SMS,
+            permissionMessage
+          )
 
-              if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                DirectSms.sendDirectSms(
-                  '09177456123',
-                  simpleCrypto.encrypt(JSON.stringify(payload))
-                )
-                showToast('Message has been sent', 'success')
-              } else {
-                throw new Error('Cannot send SMS')
-              }
-            } else {
-              // TODO: test on real device
-              const isAvailable = await SMS.isAvailableAsync()
-              if (!isAvailable) {
-                throw new Error('Cannot send SMS')
-              }
-
-              await SMS.sendSMSAsync(
-                '09177456123',
-                simpleCrypto.encrypt(JSON.stringify(payload))
-              )
-              showToast('Message has been sent', 'success')
-            }
-
-            const emergencyArray = await SecureStore.getItemAsync(
-              WIWU_OFFLINE_EMERGENCY_ARRAY
-            )
-
-            if (emergencyArray) {
-              const arr = JSON.parse(emergencyArray)
-              arr.push(payload)
-
-              await SecureStore.setItemAsync(
-                WIWU_OFFLINE_EMERGENCY_ARRAY,
-                JSON.stringify(arr)
-              )
-            } else {
-              await SecureStore.setItemAsync(
-                WIWU_OFFLINE_EMERGENCY_ARRAY,
-                JSON.stringify([payload])
-              )
-            }
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            const secretPayload = simpleCrypto.encrypt(JSON.stringify(payload))
+            DirectSms.sendDirectSms(smsReceiver, secretPayload)
+            showToast('Message has been sent', 'success')
           } else {
-            const emergency = await firestore
-              .collection('emergencies')
-              .add(payload)
-
-            await firestore
-              .collection('users')
-              .doc(uid)
-              .update({
-                emergencies: firebase.firestore.FieldValue.arrayUnion(emergency)
-              })
+            throw new Error('Cannot send SMS')
+          }
+        } else {
+          // if device is not android; TODO: test on real device
+          const isAvailable = await SMS.isAvailableAsync()
+          if (!isAvailable) {
+            throw new Error('Cannot send SMS')
           }
 
-          showToast('Emergency request sent!', 'success')
-          NavigationService.navigate('UserHome')
-          dispatch(createAction(SCREEN_LOADING)(false))
+          const encryptedPayload = simpleCrypto.encrypt(JSON.stringify(payload))
+          await SMS.sendSMSAsync(smsReceiver, encryptedPayload)
+          showToast('Message has been sent', 'success')
         }
-      )
+
+        // after sending sms
+        const emergencyArray = await SecureStore.getItemAsync(
+          WIWU_OFFLINE_EMERGENCY_ARRAY
+        )
+
+        if (emergencyArray) {
+          const arr = JSON.parse(emergencyArray)
+          arr.push(payload)
+
+          await SecureStore.setItemAsync(
+            WIWU_OFFLINE_EMERGENCY_ARRAY,
+            JSON.stringify(arr)
+          )
+        } else {
+          await SecureStore.setItemAsync(
+            WIWU_OFFLINE_EMERGENCY_ARRAY,
+            JSON.stringify([payload])
+          )
+        }
+      } else {
+        // if user is online
+
+        // TODO: refactor -R
+        if (values.media) {
+          // if user included a media
+          await uploadAsset(
+            'emergency-assets',
+            'image',
+            values.media,
+            async url => {
+              const payloadWithMedia = { ...payload, media: url }
+              const emergency = await firestore
+                .collection('emergencies')
+                .add(payloadWithMedia)
+              await firestore
+                .collection('users')
+                .doc(uid)
+                .update({
+                  emergencies: firebase.firestore.FieldValue.arrayUnion(
+                    emergency
+                  )
+                })
+            }
+          )
+        } else {
+          // if user did not include media
+          const emergency = await firestore
+            .collection('emergencies')
+            .add(payload)
+          await firestore
+            .collection('users')
+            .doc(uid)
+            .update({
+              emergencies: firebase.firestore.FieldValue.arrayUnion(emergency)
+            })
+        }
+      }
+      showToast('Emergency request sent!', 'success')
+      NavigationService.navigate('UserHome')
+      dispatch(createAction(SCREEN_LOADING)(false))
     } catch (e) {
       dispatch(createAction(SCREEN_LOADING)(false))
       showToast(e.message, 'error')
