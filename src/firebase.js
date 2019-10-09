75b0259ef4d8839164e6b6d47d9bd115545a0713
Mark <jardenilmark@gmail.com>
2019-10-09 15:00:30 +0800

    migrated

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,13 @@ import 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: API_KEY,
-  authDomain: 'weewoo-b9408.firebaseapp.com',
-  databaseURL: 'https://weewoo-b9408.firebaseio.com',
-  projectId: 'weewoo-b9408',
-  storageBucket: 'gs://weewoo-b9408.appspot.com/',
-  messagingSenderId: '363729210043',
-  appId: '1:363729210043:web:47b0c446dc062344'
+  authDomain: 'wiwu-48f9e.firebaseapp.com',
+  databaseURL: 'https://wiwu-48f9e.firebaseio.com',
+  projectId: 'wiwu-48f9e',
+  storageBucket: 'wiwu-48f9e.appspot.com',
+  messagingSenderId: '900668914554',
+  appId: '1:900668914554:web:90b417061296d685e674c3',
+  measurementId: 'G-55KP1BV024'
 }
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
