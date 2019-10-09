7276877cc9a171bf09edcd8bb4bc6e012b1892d5
Mark <jardenilmark@gmail.com>
2019-10-09 13:50:53 +0800

    fix(permissions): ios

--- src/helpers/permission.helper.js
+++ src/helpers/permission.helper.js
@@ -1,18 +1,28 @@
-import { PermissionsAndroid } from 'react-native'
+import { PermissionsAndroid, Platform } from 'react-native'
+import * as Permissions from 'expo-permissions'
+
 import showToast from './toast.helper'
 
 export const requestCamera = async () => {
-  const grantedCamera = await PermissionsAndroid.request(
-    PermissionsAndroid.PERMISSIONS.CAMERA,
-    {
-      title: 'Wiwu App Camera Permission',
-      message: 'Wiwu needs access to your camera so you can join the video',
-      buttonNeutral: 'Ask Me Later',
-      buttonNegative: 'Cancel',
-      buttonPositive: 'OK'
-    }
-  )
-  if (grantedCamera === PermissionsAndroid.RESULTS.GRANTED) {
+  const grantedCamera =
+    Platform.OS === 'ios'
+      ? await Permissions.askAsync(Permissions.CAMERA)
+      : await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          {
+            title: 'Wiwu App Camera Permission',
+            message:
+              'Wiwu needs access to your camera so you can join the video',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK'
+          }
+        )
+  if (
+    Platform.OS === 'ios'
+      ? grantedCamera.status === 'granted'
+      : grantedCamera === PermissionsAndroid.RESULTS.GRANTED
+  ) {
     return true
   } else {
     showToast('Camera permission denied')
@@ -21,19 +31,26 @@ export const requestCamera = async () => {
 }
 
 export const requestMicrophone = async () => {
-  const grantedAudio = await PermissionsAndroid.request(
-    PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
-    {
-      title: 'Wiwu App Microphone Permission',
-      message:
-        'Wiwu App needs access to your microphone ' +
-        'to speak with the verification admin',
-      buttonNeutral: 'Ask Me Later',
-      buttonNegative: 'Cancel',
-      buttonPositive: 'OK'
-    }
-  )
-  if (grantedAudio === PermissionsAndroid.RESULTS.GRANTED) {
+  const grantedAudio =
+    Platform.OS === 'ios'
+      ? await Permissions.askAsync(Permissions.AUDIO_RECORDING)
+      : await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+          {
+            title: 'Wiwu App Microphone Permission',
+            message:
+              'Wiwu App needs access to your microphone ' +
+              'to speak with the verification admin',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK'
+          }
+        )
+  if (
+    Platform.OS === 'ios'
+      ? grantedAudio.status === 'granted'
+      : grantedAudio === PermissionsAndroid.RESULTS.GRANTED
+  ) {
     return true
   } else {
     showToast('Microphone permission denied')
